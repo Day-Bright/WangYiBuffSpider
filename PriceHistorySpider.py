0a62a251bd64d7ee63b0feb700aed48864a56b58
X <56810362+Day-Bright@users.noreply.github.com>
2021-08-29 22:28:12 +0800

    Update PriceHistorySpider.py

--- PriceHistorySpider.py
+++ PriceHistorySpider.py
@@ -1,24 +1,42 @@
 # coding=utf-8
 
 import json
+import random
+
 import requests
 import time
 import DrawPriceCurve
+import USER_AGENT
 
-url = "https://buff.163.com/api/market/goods/price_history/buff"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
-    'Cookie': 'xxxxxxx'
-}
-millis = int(round(time.time() * 1000))
-param = {
-    'game': 'csgo',
-    'goods_id': '836044',
-    'currency': 'CNY',
-    'days': '30',
-    '_': millis
-}
 
-response = requests.get(url=url, params=param, headers=headers).json()
-d = DrawPriceCurve.DrawPriceCurve(response)
-d.draw()
+def get_response(goods_id):
+    url = "https://buff.163.com/api/market/goods/price_history/buff"
+    headers = {
+        'User-Agent': random.choice(USER_AGENT.user_agent_list),
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'zh-CN',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Referer': 'https://buff.163.com/goods/{goods_id}'.format(goods_id=goods_id),
+        'Cookie': 'xxx'
+    }
+    millis = int(round(time.time() * 1000))
+    param = {
+        'game': 'csgo',
+        'goods_id': goods_id,
+        'currency': 'CNY',
+        'days': '30',
+        '_': millis
+    }
+    response = requests.get(url=url, params=param, headers=headers).json()
+    return response
+
+
+if __name__ == "__main__":
+    with open(r'C:\Users\Me\Desktop\BuffSpider\items_id\Pass.json', 'r', encoding='utf-8') as f:
+        goods_dict = json.load(f)
+    for key, value in goods_dict.items():
+        response = get_response(goods_id=key)
+        d = DrawPriceCurve.DrawPriceCurve(response, value)
+        d.draw()
+
